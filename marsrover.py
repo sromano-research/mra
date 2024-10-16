a9000d25c02d0ad8157eb4e21cc167b365cffa51
sromano87 <siromano@unisa.it>
2024-10-16 10:57:55 +0200

    Add source files

--- marsrover.py
+++ marsrover.py
@@ -3,7 +3,7 @@ from error import MarsRoverError
 
 class MarsRover:
 
-    def __init__(self, planet_length: int, planet_width: int, planet_obstacles: str):
+    def __init__(self, planet_length: int, planet_width: int, planet_obstacles: list[str]):
         pass
 
     def planet_contains_obstacle_at(self, x: int, y: int) -> bool:
